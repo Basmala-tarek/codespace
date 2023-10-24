6ddfe59fce70ff9469a8ffb236a95cfeca82e10d
Basmala Qaysson <basmala@aucegypt.edu>
2023-10-24 20:37:13 +0000

    Tue, Oct 24, 2023, 8:37 PM +00:00

--- Baq/BaQaysso.py
+++ Baq/BaQaysso.py
@@ -1,4 +1,3 @@
-print ("hello, world")
 x = int(input("whats x?"))
 y = int(input("whats y?"))
 
@@ -8,3 +7,7 @@ if x > y:
 
 if x == y:
     print("x is equal to y")
+
+else:
+    print("x is less than y")
+
