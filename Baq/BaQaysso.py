6999c8d31baa48e995892d0c8435fca83e02636d
Basmala Qaysson <basmala@aucegypt.edu>
2023-10-24 22:05:08 +0000

    Tue, Oct 24, 2023, 10:05 PM +00:00

--- Baq/BaQaysso.py
+++ Baq/BaQaysso.py
@@ -1,7 +1,8 @@
 name = input("whos living there?")
 
-if name == "Harry" or name == "hermonie" or name == "Kate":
-    print ("Gryffindor")
-else:
-    print ("UCL")
+match name:
+    case "Harry":
+        print ("Gryffindor")
+    case "Hermonie":
+        print ("Gryfindor")
 
